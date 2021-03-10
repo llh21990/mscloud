79a8bccacefe75a0b9a907bd48c1aed0e1092981
llh <597796849@qq.com>
2021-03-11 01:52:17 +0800

    完成payment provider

--- cloud-provider-payment8001/src/main/java/com/llh/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/llh/springcloud/PaymentMain8001.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentProviderApplication {
+public class PaymentMain8001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentProviderApplication.class, args);
+        SpringApplication.run(PaymentMain8001.class, args);
     }
 
 }
