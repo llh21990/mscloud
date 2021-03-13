daea939c507e0319567e2d98ed13115b6cd7b08a
llh <597796849@qq.com>
2021-03-14 01:10:52 +0800

    服务提供者集群，以及resttemplate+loadbanlanced以微服务名称负载均衡请求

---
+++ cloud-provider-payment8002/src/main/java/com/llh/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.llh.springcloud.service.impl;
+
+import com.llh.springcloud.dao.PaymentDao;
+import com.llh.springcloud.entities.Payment;
+import com.llh.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getById(Long id) {
+        return paymentDao.getById(id);
+    }
+}
