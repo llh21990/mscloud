7662bbcf7f9fae9d933e035c8740aa83d02ae467
llh <597796849@qq.com>
2021-03-17 00:14:35 +0800

    替换ribbon负载均衡算法

---
+++ cloud-consumer-order80/src/main/java/com/llh/myrule/MyRule.java
@@ -0,0 +1,15 @@
+package com.llh.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyRule {
+
+    @Bean
+    public IRule myRandomRule() {
+        return new RandomRule();//定义为随机
+    }
+}
